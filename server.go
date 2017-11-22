3ee33d3dccfb6b759ce01061354195d87d36c7a5
bhaskarsaraogi <bhaskar.saraogi@gmail.com>
2017-11-22 22:54:49 +0530

    add counter metric

--- server.go
+++ server.go
@@ -7,16 +7,22 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"time"
+	"strconv"
 )
 
-func HelloWorld(histogram *prometheus.HistogramVec) http.HandlerFunc {
+const INSTANCE = "prod0"
+
+func HelloWorld(histogram *prometheus.HistogramVec, counter *prometheus.CounterVec) http.HandlerFunc {
 	return func (res http.ResponseWriter, req *http.Request) {
 		start := time.Now()
 
 
 		defer func() {
 			duration := time.Since(start)
-			histogram.WithLabelValues(fmt.Sprintf("%d", http.StatusOK)).Observe(duration.Seconds())
+			histogram.With(prometheus.Labels{
+				"status": strconv.Itoa(http.StatusOK),
+				"instance": INSTANCE}).Observe(duration.Seconds())
+			counter.With(prometheus.Labels{"instance":INSTANCE}).Inc()
 		}()
 
 		log.Println("Hello World!")
@@ -30,11 +36,17 @@ func main() {
 	histogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "hello_world_seconds",
 		Help: "Time taken to return hello world",
-	}, []string{"code"})
+	}, []string{"status", "instance"})
+
+	counter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "hello_world_requests_count",
+		Help: "Number of hellow world requests recvd",
+	}, []string{"instance"})
 
 	prometheus.Register(histogram)
+	prometheus.Register(counter)
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/hello", HelloWorld(histogram))
+	http.HandleFunc("/hello", HelloWorld(histogram, counter))
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
