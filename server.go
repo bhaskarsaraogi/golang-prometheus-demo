a1faeb83689ba2a57990001e0ea8e41782532da4
bhaskarsaraogi <bhaskar.saraogi@gmail.com>
2017-11-22 23:56:51 +0530

    add gauge metric

--- server.go
+++ server.go
@@ -8,11 +8,12 @@ import (
 	"log"
 	"time"
 	"strconv"
+	"runtime"
 )
 
 const INSTANCE = "prod0"
 
-func HelloWorld(histogram *prometheus.HistogramVec, counter *prometheus.CounterVec) http.HandlerFunc {
+func HelloWorld(histogram *prometheus.HistogramVec, counter *prometheus.CounterVec, gauge *prometheus.GaugeVec) http.HandlerFunc {
 	return func (res http.ResponseWriter, req *http.Request) {
 		start := time.Now()
 
@@ -23,6 +24,11 @@ func HelloWorld(histogram *prometheus.HistogramVec, counter *prometheus.CounterV
 				"status": strconv.Itoa(http.StatusOK),
 				"instance": INSTANCE}).Observe(duration.Seconds())
 			counter.With(prometheus.Labels{"instance":INSTANCE}).Inc()
+
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+
+			gauge.With(prometheus.Labels{"instance":INSTANCE}).Set(float64(m.Alloc / 1024))
 		}()
 
 		log.Println("Hello World!")
@@ -43,10 +49,16 @@ func main() {
 		Help: "Number of hellow world requests recvd",
 	}, []string{"instance"})
 
+	gauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "used_memory",
+		Help: "Head used memory footprint in kilobytes",
+	}, []string{"instance"})
+
 	prometheus.Register(histogram)
 	prometheus.Register(counter)
+	prometheus.Register(gauge)
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/hello", HelloWorld(histogram, counter))
+	http.HandleFunc("/hello", HelloWorld(histogram, counter, gauge))
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
